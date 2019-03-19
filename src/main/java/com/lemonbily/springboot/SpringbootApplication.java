b73b086520c8950a98bd76c91a19a774944ade12
kiboooo <kiboooo78@gmail.com>
2019-03-19 15:46:27 +0800

    增加执行类mapper地址的注解，解决主程序运行找不到类的运行错误！

--- src/main/java/com/lemonbily/springboot/SpringbootApplication.java
+++ src/main/java/com/lemonbily/springboot/SpringbootApplication.java
@@ -1,12 +1,13 @@
 package com.lemonbily.springboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.lemonbily.springboot.controller" })
+@MapperScan("com.lemonbily.springboot.mapper")//此处填写 mapper接口所在包，如果不加则需要在每个 mapper上添加 @Mapper 注解。
 
 public class SpringbootApplication {
 
